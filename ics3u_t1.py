fc67515397e17dcdf85dd525f318e9af3f9e236b
ajugoon <54783263+ajugoon@users.noreply.github.com>
2019-09-12 11:46:09 -0400

    Add files via upload

---
+++ ics3u_t1.py
@@ -0,0 +1,12 @@
+number1 = input("What is your first number?")
+print("Your first number is " + number1)
+
+number2 = input("What is your second number?")
+print("Your second number is " + number2)
+
+number3 = input("What is your third number?")
+print("Your third number is " + number3)
+
+average = (int(number1) + int(number2) + int(number3))/3
+
+print("The average is " + str(average))
---
+++ ics3u_t2.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+year = input("What year were you born? ")
+age = 2019 - int(year)
+print("You are " + str(age) + " years old, " + name + "!")
